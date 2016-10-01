3949e7dc96f94e78751a6954ae366b2dbd7b41fd
Rushi Shah <2016rshah@gmail.com>
2016-10-01 14:01:17 -0500

    Only one error left in this file for this port

--- src/Data/Romefile.hs
+++ src/Data/Romefile.hs
@@ -8,7 +8,7 @@ module Data.Romefile
     , romefile
     , RomefileEntry (..)
     , FrameworkName
-    , GitRepoName
+    , GitRepoName (..)
     )
 where
 
@@ -23,7 +23,8 @@ import           Control.Monad.Trans
 
 
 type FrameworkName = String
-type GitRepoName   = String
+newtype GitRepoName = GitRepoName { unGitRepoName :: String } 
+                    deriving (Eq, Show)
 data RomefileEntry = RomefileEntry { gitRepositoryName   :: GitRepoName
                                    , frameworkCommonNames :: [FrameworkName]
                                    }
@@ -64,4 +65,13 @@ getBucket ini = requireKey s3BucketKey `inRequiredSection` cacheSectionDelimiter
 
 getRomefileEntries ini = do
   m <- inOptionalSection repositoryMapSectionDelimiter M.empty keysAndValues `fromIni''` ini
-  return $ Prelude.map (\(repoName, frameworkCommonNames) -> RomefileEntry (unpack repoName) (Prelude.map (unpack . strip) (splitOn "," frameworkCommonNames))) (M.toList m)
+
+  return $
+    Prelude.map
+    (\(repoName, frameworkCommonNames)
+     -> RomefileEntry
+        (GitRepoName (unpack repoName))
+        (Prelude.map
+         (unpack . strip)
+         (splitOn "," frameworkCommonNames)))
+    (M.toList m)

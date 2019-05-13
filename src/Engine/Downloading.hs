45a3fda317e3da013a1687a8a6366f88484b726f
Patrick Balestra <me@patrickbalestra.com>
2019-05-13 22:14:20 +0200

    More WIP Engine/Downloading.hs

--- src/Engine/Downloading.hs
+++ src/Engine/Downloading.hs
@@ -39,14 +39,62 @@ getFrameworkFromEngine
        LBS.ByteString
 getFrameworkFromEngine enginePath reverseRomeMap (FrameworkVersion f@(Framework fwn _ _) version) platform
   = do
-    (CachePrefix prefix, verbose) <- ask
+    (CachePrefix cachePrefix, verbose) <- ask
+    let frameworkLocalPath = cachePrefix </> remoteFrameworkUploadPath
     mapExceptT
       (withReaderT (const (verbose)))
-      (getArtifactFromEngine enginePath (prefix </> remoteFrameworkUploadPath) fwn
+      (getArtifactFromEngine enginePath remoteFrameworkUploadPath frameworkLocalPath fwn
       )
  where
-  remoteFrameworkUploadPath =
-    remoteFrameworkPath platform reverseRomeMap f version
+  remoteFrameworkUploadPath = remoteFrameworkPath platform reverseRomeMap f version
+
+
+-- | Retrieves a .version file using the engine
+getVersionFileFromEngine
+  :: FilePath
+  -> ProjectNameAndVersion
+  -> ExceptT
+       String
+       (ReaderT (CachePrefix, Bool) IO)
+       LBS.ByteString
+getVersionFileFromEngine enginePath projectNameAndVersion = do
+  (CachePrefix prefix, verbose) <- ask
+  let finalVersionFileRemotePath = prefix </> versionFileRemotePath
+  mapExceptT (withReaderT (const (verbose))) $ getArtifactFromEngine
+    enginePath
+    finalVersionFileRemotePath
+    finalVersionFileRemotePath
+    versionFileName
+ where
+  versionFileName = versionFileNameForProjectName $ fst projectNameAndVersion
+  versionFileRemotePath = remoteVersionFilePath projectNameAndVersion
+
+
+-- | Retrieves a bcsymbolmap with the engine
+getBcsymbolmapWithEngine
+  :: FilePath -- ^ The cache definition
+  -> InvertedRepositoryMap -- ^ The map used to resolve from a `FrameworkVersion` to the path of the dSYM in the cache
+  -> FrameworkVersion -- ^ The `FrameworkVersion` identifying the dSYM
+  -> TargetPlatform -- ^ The `TargetPlatform` to limit the operation to
+  -> DwarfUUID -- ^ The UUID of the bcsymbolmap
+  -> ExceptT
+       String
+       (ReaderT (CachePrefix, Bool) IO)
+       LBS.ByteString
+getBcsymbolmapWithEngine enginePath reverseRomeMap (FrameworkVersion f@(Framework fwn _ _) version) platform dwarfUUID
+  = do
+    (CachePrefix prefix, verbose) <- ask
+    let finalRemoteBcsymbolmaploadPath = prefix </> remoteBcSymbolmapUploadPath
+    mapExceptT (withReaderT (const (verbose))) $ getArtifactFromEngine
+      enginePath
+      finalRemoteBcsymbolmaploadPath
+      finalRemoteBcsymbolmaploadPath
+      symbolmapName
+ where
+  remoteBcSymbolmapUploadPath =
+    remoteBcsymbolmapPath dwarfUUID platform reverseRomeMap f version
+  symbolmapName = fwn <> "." <> bcsymbolmapNameFrom dwarfUUID
+
 
 -- | Retrieves a dSYM using the engine
 getDSYMFromEngine
@@ -63,54 +111,76 @@ getDSYMFromEngine enginePath reverseRomeMap (FrameworkVersion f@(Framework fwn _
     (CachePrefix prefix, verbose) <- ask
     let finalRemoteDSYMUploadPath = prefix </> remoteDSYMUploadPath
     mapExceptT (withReaderT (const (verbose)))
-      $ getArtifactFromEngine enginePath finalRemoteDSYMUploadPath dSYMName
+      $ getArtifactFromEngine enginePath finalRemoteDSYMUploadPath finalRemoteDSYMUploadPath dSYMName -- TODO: pass local DSYM FilePath
  where
   remoteDSYMUploadPath = remoteDsymPath platform reverseRomeMap f version
   dSYMName             = fwn <> ".dSYM"
 
--- getVersionFileFromEngine
--- | Retrieves a .version file using the engine
-getVersionFileFromEngine
-  :: FilePath
-  -> ProjectNameAndVersion
-  -> ExceptT
-       String
-       (ReaderT (CachePrefix, Bool) IO)
-       LBS.ByteString
-getVersionFileFromEngine enginePath projectNameAndVersion = do
-  (CachePrefix prefix, verbose) <- ask
-  let finalVersionFileRemotePath = prefix </> versionFileRemotePath
-  mapExceptT (withReaderT (const (verbose))) $ getArtifactFromEngine
-    enginePath
-    finalVersionFileRemotePath
-    versionFileName
- where
-  versionFileName = versionFileNameForProjectName $ fst projectNameAndVersion
-  versionFileRemotePath = remoteVersionFilePath projectNameAndVersion
 
--- | Retrieves a bcsymbolmap using the engine
-getBcsymbolmapFromEngine
-  :: FilePath -- ^ The cache definition
+-- | Retrieves a bcsymbolmap using the engine and unzip the contents
+getAndUnzipBcsymbolmapWithEngine
+  :: FilePath -- ^ The engine path
   -> InvertedRepositoryMap -- ^ The map used to resolve from a `FrameworkVersion` to the path of the dSYM in the cache
   -> FrameworkVersion -- ^ The `FrameworkVersion` identifying the dSYM
   -> TargetPlatform -- ^ The `TargetPlatform` to limit the operation to
   -> DwarfUUID -- ^ The UUID of the bcsymbolmap
-  -> ExceptT
-       String
-       (ReaderT (CachePrefix, Bool) IO)
-       LBS.ByteString
-getBcsymbolmapFromEngine enginePath reverseRomeMap (FrameworkVersion f@(Framework fwn _ _) version) platform dwarfUUID
-  = do
-    (CachePrefix prefix, verbose) <- ask
-    let finalRemoteBcsymbolmaploadPath = prefix </> remoteBcSymbolmapUploadPath
-    mapExceptT (withReaderT (const (verbose))) $ getArtifactFromEngine
-      enginePath
-      finalRemoteBcsymbolmaploadPath
-      symbolmapName
+  -> ExceptT String (ReaderT (CachePrefix, Bool) IO) ()
+getAndUnzipBcsymbolmapWithEngine enginePath reverseRomeMap fVersion@(FrameworkVersion f@(Framework fwn _ fwps) version) platform dwarfUUID
+  = when (platform `elem` fwps) $ do
+    (_, verbose) <- ask
+    let symbolmapName = fwn <> "." <> bcsymbolmapNameFrom dwarfUUID
+    binary <- getBcsymbolmapWithEngine enginePath
+                                       reverseRomeMap
+                                       fVersion
+                                       platform
+                                       dwarfUUID
+    deleteFile (bcsymbolmapPath dwarfUUID) verbose
+    unzipBinary binary symbolmapName (bcsymbolmapZipName dwarfUUID) verbose
  where
-  remoteBcSymbolmapUploadPath =
-    remoteBcsymbolmapPath dwarfUUID platform reverseRomeMap f version
-  symbolmapName = fwn <> "." <> bcsymbolmapNameFrom dwarfUUID
+  platformBuildDirectory =
+    carthageArtifactsBuildDirectoryForPlatform platform f
+  bcsymbolmapZipName d = bcsymbolmapArchiveName d version
+  bcsymbolmapPath d = platformBuildDirectory </> bcsymbolmapNameFrom d
+
+
+-- | Retrieves all the bcsymbolmap files using the engine and unzip the contents
+getAndUnzipBcsymbolmapsWithEngine'
+  :: FilePath -- ^ The cache definition
+  -> InvertedRepositoryMap -- ^ The map used to resolve from a `FrameworkVersion` to the path of the dSYM in the cache
+  -> FrameworkVersion -- ^ The `FrameworkVersion` identifying the Framework
+  -> TargetPlatform -- ^ The `TargetPlatform` to limit the operation to
+  -> ExceptT
+       DWARFOperationError
+       (ReaderT (CachePrefix, Bool) IO)
+       ()
+getAndUnzipBcsymbolmapsWithEngine' enginePath reverseRomeMap fVersion@(FrameworkVersion f@(Framework fwn _ fwps) _) platform
+  -- = undefined
+  = when (platform `elem` fwps) $ do
+
+    dwarfUUIDs <- withExceptT (const ErrorGettingDwarfUUIDs)
+      $ dwarfUUIDsFrom (frameworkDirectory </> fwn)
+    eitherDwarfUUIDsOrSucces <- forM
+      dwarfUUIDs
+      (\dwarfUUID -> lift $ runExceptT
+        (withExceptT (\e -> (dwarfUUID, e)) $ getAndUnzipBcsymbolmapWithEngine
+          enginePath
+          reverseRomeMap
+          fVersion
+          platform
+          dwarfUUID
+        )
+      )
+
+    let failedUUIDsAndErrors = lefts eitherDwarfUUIDsOrSucces
+    unless (null failedUUIDsAndErrors) $ throwError $ FailedDwarfUUIDs
+      failedUUIDsAndErrors
+ where
+  frameworkNameWithFrameworkExtension = appendFrameworkExtensionTo f
+  platformBuildDirectory =
+    carthageArtifactsBuildDirectoryForPlatform platform f
+  frameworkDirectory =
+    platformBuildDirectory </> frameworkNameWithFrameworkExtension
+
 
 -- | Retrieves a Framework using the engine and unzip the contents
 getAndUnzipFrameworkWithEngine
@@ -135,6 +205,7 @@ getAndUnzipFrameworkWithEngine enginePath reverseRomeMap fVersion@(FrameworkVers
   frameworkZipName        = frameworkArchiveName f version
   frameworkExecutablePath = frameworkBuildBundleForPlatform platform f </> fwn
 
+
 -- | Retrieves a dSYM using the engine and unzip the contents
 getAndUnzipDSYMWithEngine
   :: FilePath -- ^ The cache definition
@@ -143,59 +214,22 @@ getAndUnzipDSYMWithEngine
   -> TargetPlatform -- ^ The `TargetPlatform` to limit the operation to
   -> ExceptT String (ReaderT (CachePrefix, Bool) IO) ()
 getAndUnzipDSYMWithEngine enginePath reverseRomeMap fVersion@(FrameworkVersion f@(Framework fwn _ fwps) version) platform
-  = undefined
-  -- = when (platform `elem` fwps) $ do
-  --   (_, _, verbose) <- ask
-  --   dSYMBinary <- getDSYMFromS3 s3BucketName reverseRomeMap fVersion platform
-  --   deleteDSYMDirectory fVersion platform verbose
-  --   unzipBinary dSYMBinary fwn dSYMZipName verbose
-  -- where dSYMZipName = dSYMArchiveName f version
-
--- | Retrieves all the bcsymbolmap files using the engine and unzip the contents
-getAndUnzipBcsymbolmapsWithEngine'
-  :: FilePath -- ^ The cache definition
-  -> InvertedRepositoryMap -- ^ The map used to resolve from a `FrameworkVersion` to the path of the dSYM in the cache
-  -> FrameworkVersion -- ^ The `FrameworkVersion` identifying the Framework
-  -> TargetPlatform -- ^ The `TargetPlatform` to limit the operation to
-  -> ExceptT
-       DWARFOperationError
-       (ReaderT (CachePrefix, Bool) IO)
-       ()
-getAndUnzipBcsymbolmapsWithEngine' enginePath reverseRomeMap fVersion@(FrameworkVersion f@(Framework fwn _ fwps) _) platform
-  = undefined
- --  = when (platform `elem` fwps) $ do
-
- --    dwarfUUIDs <- withExceptT (const ErrorGettingDwarfUUIDs)
- --      $ dwarfUUIDsFrom (frameworkDirectory </> fwn)
- --    eitherDwarfUUIDsOrSucces <- forM
- --      dwarfUUIDs
- --      (\dwarfUUID -> lift $ runExceptT
- --        (withExceptT (\e -> (dwarfUUID, e)) $ getAndUnzipBcsymbolmapFromS3
- --          lCacheDir
- --          reverseRomeMap
- --          fVersion
- --          platform
- --          dwarfUUID
- --        )
- --      )
-
- --    let failedUUIDsAndErrors = lefts eitherDwarfUUIDsOrSucces
- --    unless (null failedUUIDsAndErrors) $ throwError $ FailedDwarfUUIDs
- --      failedUUIDsAndErrors
- -- where
- --  frameworkNameWithFrameworkExtension = appendFrameworkExtensionTo f
- --  platformBuildDirectory =
- --    carthageArtifactsBuildDirectoryForPlatform platform f
- --  frameworkDirectory =
- --    platformBuildDirectory </> frameworkNameWithFrameworkExtension
+  -- = undefined
+  = when (platform `elem` fwps) $ do
+    (_, verbose) <- ask
+    dSYMBinary <- getDSYMFromEngine enginePath reverseRomeMap fVersion platform
+    deleteDSYMDirectory fVersion platform verbose
+    unzipBinary dSYMBinary fwn dSYMZipName verbose
+  where dSYMZipName = dSYMArchiveName f version
 
 -- | Retrieves an artifact using the engine
 getArtifactFromEngine
   :: FilePath -- ^ The engine file path
-  -> FilePath -- ^ The path in the cache
+  -> FilePath -- ^ The remote path 
+  -> FilePath -- ^ The local path
   -> String -- ^ A colloquial name for the artifact
   -> ExceptT String (ReaderT (Bool) IO) LBS.ByteString
-getArtifactFromEngine enginePath remotePath artifactName = do
+getArtifactFromEngine enginePath remotePath localPath artifactName = do
   readerEnv@(verbose)            <- ask
   eitherArtifact <- liftIO $ try $ runReaderT
     (downloadBinaryWithEngine enginePath remotePath artifactName)
@@ -218,11 +252,21 @@ downloadBinaryWithEngine
 downloadBinaryWithEngine enginePath objectRemotePath objectName = do
     (verbose) <- ask
     let cmd = Turtle.fromString $ enginePath
+    let sayFunc = if verbose then sayLnWithTime else sayLn
+    when verbose
+      $  sayFunc
+      $  "Executing script"
+      <> (show enginePath)
+      <> " to download "
+      <> objectName
+      <> " from: "
+      <> objectRemotePath
     (exitCode) <- Turtle.proc
         cmd
-        ["download", (Turtle.fromString objectRemotePath), (Turtle.fromString objectName)]
+        ["download", (Turtle.fromString objectRemotePath), (Turtle.fromString objectRemotePath)]
         (return $ Turtle.unsafeTextToLine "")
     case exitCode of
         Turtle.ExitSuccess   -> return ()
         Turtle.ExitFailure n -> Turtle.die (cmd <> " failed with exit code: " <> Turtle.repr n)
-    undefined
+    -- undefined
+    liftIO $ LBS.readFile objectRemotePath
